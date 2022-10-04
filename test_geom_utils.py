a0e52db5ac145cc17454d0998b89dab552dba7e7
Chris Doughty <doug0181@algonquincollege.com>
2022-10-04 15:15:56 -0400

    added asserts

--- test_geom_utils.py
+++ test_geom_utils.py
@@ -10,54 +10,63 @@ def test_is_point_in_box_left_outside():
     y = 2
     print("Expected: False")
     print("Actual:" + (gu.is_point_in_box(x,y,xmin,xmax,ymin,ymax)))
+    assert False, 'Condition was true.'
 
 def test_is_point_in_box_left_edge():
     x = 1
     y = 2
     print("Expected: False")
     print("Actual:" + (gu.is_point_in_box(x,y,xmin,xmax,ymin,ymax)))
+    assert False, 'Condition was true.'
    
 def test_is_point_in_box_above_outside():
     x = 2
     y = 4
     print("Expected: False")
     print("Actual:" + (gu.is_point_in_box(x,y,xmin,xmax,ymin,ymax)))
+    assert False, 'Condition was true.'
    
 def test_is_point_in_box_top_edge():
     x = 2
     y = 3
     print("Expected: False")
     print("Actual:" + (gu.is_point_in_box(x,y,xmin,xmax,ymin,ymax)))
+    assert False, 'Condition was true.'
    
 def test_is_point_in_box_right_outside():
     x = 4
     y = 2
     print("Expected: False")
     print("Actual:" + (gu.is_point_in_box(x,y,xmin,xmax,ymin,ymax)))
+    assert False, 'Condition was true.'
    
 def test_is_point_in_box_right_edge():
     x = 3
     y = 2
     print("Expected: False")
     print("Actual:" + (gu.is_point_in_box(x,y,xmin,xmax,ymin,ymax)))
+    assert False, 'Condition was true.'
    
 def test_is_point_in_box_below_outside():
     x = 2
     y = 0
     print("Expected: False")
     print("Actual:" + (gu.is_point_in_box(x,y,xmin,xmax,ymin,ymax)))
+    assert False, 'Condition was true.'
    
 def test_is_point_in_box_bottom_edge():
     x = 2
     y = 1
     print("Expected: False")
     print("Actual:" + (gu.is_point_in_box(x,y,xmin,xmax,ymin,ymax)))
+    assert False, 'Condition was true.'
    
 def test_is_point_in_box_inside():
     x = 2
     y = 2
     print("Expected: True")
     print("Actual:" + (gu.is_point_in_box(x,y,xmin,xmax,ymin,ymax)))
+    assert True, 'Condition was false.'
 
 test_is_point_in_box_left_outside()
 test_is_point_in_box_left_edge()
