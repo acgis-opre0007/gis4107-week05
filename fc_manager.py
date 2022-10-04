db5fa09ccce2356519750f83a080abcf5f142910
Laura Oprescu <opre0007@algonquinlive.com>
2022-10-04 14:52:43 -0400

    up to 15

--- fc_manager.py
+++ fc_manager.py
@@ -1,2 +1,9 @@
 def get_feature_type(feature_code):
-    return f''
+    if 1:
+        return f'POINT'
+    if 2:
+        return f'POLYLINE'
+    if 3:
+        return f'POLYGON'
+    else:
+        return f'None'
