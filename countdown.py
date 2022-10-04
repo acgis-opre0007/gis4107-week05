f0188eee0e33bea10b7aec9f9052b7f8c4620fe1
Chris Doughty <doug0181@algonquincollege.com>
2022-10-04 16:57:33 -0400

    improved for and while loops

--- countdown.py
+++ countdown.py
@@ -4,11 +4,14 @@ def get_countdown_as_text_using_for(start_value=10):
        containing numbers descending from start_value to 0 using a for loop
     """
 
-    for x in range(start_value,0,-1):
+    for x in range(start_value,-1,-1):
       print(x)
 
 def get_countdown_as_text_using_while(start_value=10):
     """Given a start_value, default is 10, return a space delimited string
        containing numbers descending from start_value to 0 using a while loop
     """
-    return ''
+    
+    while start_value > -1:
+      print(start_value)
+      start_value -= 1
--- test_countdown.py
+++ test_countdown.py
@@ -1,19 +1,24 @@
 import countdown as c
 
-def test_countdown_for():
-    for start_value in range(1,10):
-        if (start_value < 1): break
-        if <>: continue
-        <>
-    else:
-        print (f'Loop done.')
+def test_countdown_for_10():
+    """Tests for loop in countdown.py with default value; i.e. no argument."""
+    c.get_countdown_as_text_using_for()
 
+def test_countdown_for_5():
+    """Tests for loop in countdown.py with starting value of 5."""
+    count_from = 5
+    c.get_countdown_as_text_using_for(count_from)
 
-def test_countdown_while():
-    while <>:
-        <>
-        if <>: break
-        if <>: continue
-        <>
-    else:
-        print (f'Loop done.')
+def test_countdown_while_10():
+    """Tests for while in countdown.py with default value; i.e. no argument."""
+    c.get_countdown_as_text_using_while()
+
+def test_countdown_while_5():
+    """Tests for while in countdown.py with starting value of 5."""
+    count_from = 5
+    c.get_countdown_as_text_using_while(count_from)
+
+test_countdown_for_10()
+test_countdown_for_5()
+test_countdown_while_10()
+test_countdown_while_5()
