72e989e1d17437d2d974d314a5b39357abbc1958
Laura Oprescu <opre0007@algonquinlive.com>
2022-10-04 16:02:52 -0400

    .

---
+++ test_countdown.py
@@ -0,0 +1,19 @@
+import countdown as c
+
+def test_countdown_for():
+    for start_value in range(1,10):
+        if (start_value < 1): break
+        if <>: continue
+        <>
+    else:
+        print (f'Loop done.')
+
+
+def test_countdown_while():
+    while <>:
+        <>
+        if <>: break
+        if <>: continue
+        <>
+    else:
+        print (f'Loop done.')
