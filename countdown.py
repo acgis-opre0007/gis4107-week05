ac7300025cf44aba134802ecaea07f401b1fc050
Chris Doughty <doug0181@algonquincollege.com>
2022-10-04 17:15:30 -0400

    formatted strings being returned properly

--- countdown.py
+++ countdown.py
@@ -3,15 +3,20 @@ def get_countdown_as_text_using_for(start_value=10):
     """Given a start_value, default is 10, return a space delimited string
        containing numbers descending from start_value to 0 using a for loop
     """
-
+    result = ''
     for x in range(start_value,-1,-1):
-      print(x)
+       result += f'{x} '
+    return result
 
 def get_countdown_as_text_using_while(start_value=10):
     """Given a start_value, default is 10, return a space delimited string
        containing numbers descending from start_value to 0 using a while loop
     """
-    
+    result = ''
     while start_value > -1:
-      print(start_value)
+      result += f'{start_value} '
       start_value -= 1
+    return result
+
+print(get_countdown_as_text_using_for())
+print(get_countdown_as_text_using_while())
