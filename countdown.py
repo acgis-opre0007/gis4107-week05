1a0f19570bb2609aa0f532403cd5db99272e91fe
Chris Doughty <doug0181@algonquincollege.com>
2022-10-04 16:31:26 -0400

    initial countdown for loop

--- countdown.py
+++ countdown.py
@@ -3,11 +3,15 @@ def get_countdown_as_text_using_for(start_value=10):
     """Given a start_value, default is 10, return a space delimited string
        containing numbers descending from start_value to 0 using a for loop
     """
-    return ''
+
+    for x in range(start_value):
+      print(start_value - x)
 
 def get_countdown_as_text_using_while(start_value=10):
     """Given a start_value, default is 10, return a space delimited string
        containing numbers descending from start_value to 0 using a while loop
     """
     return ''
+
+get_countdown_as_text_using_for()
 
